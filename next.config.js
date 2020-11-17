03bb2c7a9505ea5873b686dea6afbafa4618f1f6
Sergio Cordero <sergio.crol@gmail.com>
2020-11-17 20:06:26 +0100

    config

--- next.config.js
+++ next.config.js
@@ -5,16 +5,4 @@ module.exports = {
   exportPathMap: () => ({
     '/': { page: '/' },
   }),
-  assetPrefix: isProd ? '/lolo-landing-page' : '',
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      issuer: {
-        test: /\.(js|ts)x?$/,
-      },
-      use: ['@svgr/webpack'],
-    });
-
-    return config;
-  },
 };
