91f693e367745d7c05292e3db97e8650ffaf2ca4
Sergio Cordero <sergio.crol@gmail.com>
2020-11-17 19:16:48 +0100

    config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const isProd = (process.env.NODE_ENV || 'production') === 'production';
 
 module.exports = {
+  target: 'serverless',
   exportPathMap: () => ({
     '/': { page: '/' },
   }),
--- out/_next/static/chunks/webpack.js
+++ out/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c367819083344d11dcf2";
+/******/ 	var hotCurrentHash = "6ef50ffe7bda6c64290c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
