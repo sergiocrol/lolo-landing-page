f8d9531c0975c83d977d9c01536002980f4161f8
Sergio Cordero <sergio.crol@gmail.com>
2021-01-10 20:35:01 +0100

    Changes in the menu, styles and i18n added

--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@ const isProd = (process.env.NODE_ENV || 'production') === 'production';
 
 module.exports = {
   // target: 'serverless',
-  exportPathMap: () => ({
-    '/': { page: '/' },
-  }),
+  // exportPathMap: () => ({
+  //   '/': { page: '/' },
+  // }),
   i18n: {
     locales: ['es', 'ca'],
     defaultLocale: 'es',
