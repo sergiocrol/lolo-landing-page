96852f6ae542266b4abb41304dc0aef779f22db9
sergiocrol <sergio.crol@gmail.com>
2021-01-24 07:46:13 +0100

    Styles changes test

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
     // removeDeprecatedGapUtilities: true,
     // purgeLayersByDefault: true,
   },
-  purge: ['./components/**/*.js', './pages/**/*.js'],
+  // purge: ['./components/**/*.js', './pages/**/*.js'],
   theme: {
     extend: {
       colors: {
